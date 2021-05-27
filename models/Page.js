b85cf141c39654449db4fd969762537dfc41b24a
Rafael <rafavillalopez@gmail.com>
2021-05-27 10:39:41 -0300

    Working on test and some changes

--- models/Page.js
+++ models/Page.js
@@ -1,5 +1,7 @@
 const S = require("sequelize");
 const db = require("../db");
+const marked = require("marked");
+const Tag = require("./Tag");
 
 class Page extends S.Model {}
 
@@ -19,11 +21,22 @@ Page.init(
     },
     status: {
       type: S.ENUM("open", "closed"),
+      validate: {
+        isIn: {
+          args: [["open", "closed"]],
+        },
+      },
     },
     route: {
       type: S.VIRTUAL,
       get() {
         return `/wiki/${this.getDataValue("urlTitle")}`;
+      },
+    },
+    renderedContent: {
+      type: S.VIRTUAL,
+      get() {
+        return marked(this.getDataValue("content"));
       },
     },
   },
@@ -46,5 +59,35 @@ Page.addHook("beforeValidate", (page, options) => {
   const url = generateUrlTitle(page.title);
   page.urlTitle = url;
 });
+
+Page.findByTag = async function (name) {
+  let tag = await Tag.findOne({ where: { name } });
+  if (tag) {
+    let page = await tag.getPages();
+    return page;
+  } else return [];
+};
+
+Page.prototype.findSimilar = async function () {
+  const tags = await this.getTags();
+
+  let pagesForTag = await Promise.all(
+    tags.map((tag) => {
+      return tag.getPages();
+    })
+  );
+  const pagesIds = [];
+  pagesForTag = pagesForTag.flat().forEach((pageOfTag) => {
+    let id = pageOfTag.dataValues.id;
+
+    if (!(pagesIds.includes(id) || id === this.dataValues.id))
+      pagesIds.push(id);
+  });
+  return await Promise.all(
+    pagesIds.map((id) => {
+      return Page.findByPk(id);
+    })
+  );
+};
 
 module.exports = Page;
